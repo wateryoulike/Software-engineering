a7434c48be05c82a945b79f2099e593f9aaf6fc9
wateryoulike <1519086312@example.com>
2020-04-28 11:11:57 +0800

    第五次上传
    增加前台模板样式，模板和视图初步连接，初步显示基本网页框架

--- softwareEngineering/rearend/views.py
+++ softwareEngineering/rearend/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def workerList(request):
+    return render(request, 'workerList.html')
+
+def calculation(request):
+    return render(request, 'calculation.html')
+
+def aboutUs(request):
+    return render(request, 'aboutUs.html')
