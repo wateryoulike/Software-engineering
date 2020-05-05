b2648940956634ecdb88a49a3452cb992dd7a48e
wateryoulike <45584212+wateryoulike@users.noreply.github.com>
2020-05-06 01:06:40 +0800

    数据备份
    数据备份

--- 数据库文件/engineer.sql
+++ 数据库文件/engineer.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : test
+ Source Server         : local
  Source Server Type    : MySQL
  Source Server Version : 50724
  Source Host           : localhost:3306
@@ -11,7 +11,7 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 27/04/2020 00:12:42
+ Date: 06/05/2020 00:26:27
 */
 
 SET NAMES utf8mb4;
@@ -22,8 +22,8 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `engineer`;
 CREATE TABLE `engineer`  (
-  `id` int(11) NOT NULL,
-  `number` int(11) NULL DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `sex` int(1) NULL DEFAULT NULL,
   `birth_date` datetime(0) NULL DEFAULT NULL,
@@ -34,6 +34,13 @@ CREATE TABLE `engineer`  (
   `seniority` int(4) NULL DEFAULT NULL,
   `basic_wage` double(255, 0) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of engineer
+-- ----------------------------
+INSERT INTO `engineer` VALUES (1, '20170528', '张贺', 1, '1982-04-01 00:00:00', 2, '嘉兴', '杭州拱墅区', '17322861234', 13, 15400);
+INSERT INTO `engineer` VALUES (2, '20170527', '熊星琼', 1, '1978-11-03 00:00:00', 1, '绍兴', '杭州西湖区', '17322861458', 20, 21400);
+INSERT INTO `engineer` VALUES (3, '20170565', '袁惠敏', 0, '1989-05-15 00:00:00', 2, '杭州', '杭州临安区', '17322864874', 9, 11500);
 
 SET FOREIGN_KEY_CHECKS = 1;
